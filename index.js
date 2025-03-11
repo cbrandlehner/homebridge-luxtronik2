81599390d0fe3ea6391af442551979b580bc346f
cbrandlehner <christian@brandlehner.at>
2025-03-11 21:10:19 +0100

    1.3.0

--- index.js
+++ index.js
@@ -8,43 +8,47 @@ const packageFile = require('./package.json');
 function translate(c) {
   const tools = require(path.join(__dirname, '/lib/tools.js'));
   const channellist = require(path.join(__dirname, '/lib/channellist.json'));
-	// translate dword to data type
-		const result = [];
-		for (let i = 0; i < c.length; i++) {
-			if (tools.isset(channellist[i])) {
-				let value = c[i];
-				if (tools.isset(channellist[i].type)) {
-					switch (channellist[i].type) {
-					case 'fix1': {
-						value /= 10;
-            break;}
+  const result = [];
 
-            case 'ip': {
-						value = tools.int2ip(value);
-            break;}
+  for (let i = 0; i < c.length; i++) {
+    if (tools.isset(channellist[i])) {
+      let value = c[i];
+      if (tools.isset(channellist[i].type)) {
+        switch (channellist[i].type) {
+          case 'fix1': {
+            value /= 10;
+            break;
+          }
 
-            case 'ignore': {
-            continue;}
+          case 'ip': {
+            value = tools.int2ip(value);
+            break;
+          }
 
-            case 'enum': {
-						if (tools.isset(channellist[i].enum[c[i]])) value = channellist[i].enum[c[i]];
-            break;}
+          case 'ignore': {
+            continue;
+          }
 
-            default: {
-            // nothing
-            break;}
-					}
-				}
+          case 'enum': {
+            if (tools.isset(channellist[i].enum[c[i]])) value = channellist[i].enum[c[i]];
+            break;
+          }
 
-			result.push(value); // push to array
-			}
-		}
+          default: {
+            break;
+          }
+        }
+      }
 
-		return result;
-	}	// function translate(c)
+      result.push(value);
+    }
+  }
+
+  return result;
+}
 
 function Luxtronik2(log, config) {
-	this.log = log;
+  this.log = log;
 
   const process = require('node:process');
   const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
@@ -59,7 +63,6 @@ function Luxtronik2(log, config) {
     this.IP = '127.0.0.1';
   } else {
     this.IP = config.IP;
-    this.log.debug('Config: IP is %s', config.IP);
   }
 
   if (config.Port === undefined) {
@@ -67,7 +70,6 @@ function Luxtronik2(log, config) {
     this.Port = 8888;
   } else {
     this.Port = config.Port;
-    this.log.debug('Config: Port is %s', config.Port);
   }
 
   if (config.name === undefined) {
@@ -75,7 +77,6 @@ function Luxtronik2(log, config) {
     this.name = 'Unnamed Luxtronik2';
   } else {
     this.name = config.name;
-    this.log.debug('Config: name is %s', config.name);
   }
 
   if (config.Channel === undefined) {
@@ -83,10 +84,14 @@ function Luxtronik2(log, config) {
     this.Channel = 5;
   } else {
     this.Channel = config.Channel;
-    this.log.debug('Config: Channel is %s', config.Channel);
   }
 
-  this.CurrentTemperature = 0; // Initial value for early response
+  this.log.debug('Config: IP is %s', this.IP);
+  this.log.debug('Config: Port is %s', this.Port);
+  this.log.debug('Config: name is %s', this.name);
+  this.log.debug('Config: Channel is %s', this.Channel);
+
+  this.CurrentTemperature = 0;
   this.counter = 1;
 
   this.temperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
@@ -112,49 +117,39 @@ Luxtronik2.prototype = {
     const luxsock = net.connect({host: this.IP, port: this.Port});
 
     this.log.debug('Going to connect');
-		luxsock.on('error', function (data) {
-			that.log.error(data.toString());
-			// stop();
-		});
-		/* handle timeout */
-		luxsock.on('timeout', function () {
-			that.log.warn('Connection timeout. Check network settings.');
-			// stop();
-		});
-		/* handle close */
-		luxsock.on('close', function () {
+    luxsock.on('error', function (data) {
+      that.log.error(data.toString());
+    });
+    luxsock.on('timeout', function () {
+      that.log.warn('Connection timeout. Check network settings.');
+    });
+    luxsock.on('close', function () {
       that.log.debug('Connection to Luxtronik2 closed.');
-			// stop();
-		});
-		/* handle end */
-		luxsock.on('end', function () {
-			that.log.debug('Connection to Luxtronik2 ended.');
-			// stop();
-		});
-		/* receive data */
-		luxsock.on('data', function (data) {
-			that.log.debug('Connection to Luxtronik2 established. Requesting data by sending command.');
+    });
+    luxsock.on('end', function () {
+      that.log.debug('Connection to Luxtronik2 ended.');
+    });
+    luxsock.on('data', function (data) {
+      that.log.debug('Connection to Luxtronik2 established. Requesting data by sending command.');
       const {Buffer} = require('node:buffer');
       const buf = Buffer.alloc(data.length);
-			buf.write(data, 'binary');
-			/* luxtronik must confirm command */
+      buf.write(data, 'binary');
       that.log.debug('Buffer length is %s', buf.length);
-			const confirm = buf.readUInt32BE(0);
+      const confirm = buf.readUInt32BE(0);
       that.log.debug('Confirm message is %s', confirm);
-			/* is 0 if data is unchanged since last request */
       const offset = 8;
       if (offset + 4 > buf.length) {
-        // buffer does not have enough bytes to read uint32 at offset
         that.log.warn('Buffer does not have enough bytes. Exiting function without being able to update data.');
+        return;
       }
 
-			if ((confirm === 3004) && (offset + 4 <= buf.length)) {
+      if (confirm === 3004 && offset + 4 <= buf.length) {
         that.log.debug('Luxtronik2 confirmed command and the buffer byte count is good.');
         const count = buf.readUInt32BE(offset);
         if (data.length === (count * 4) + 12) {
           let pos = 12;
-           const calculated = new Int32Array(count);
-            for (let i = 0; i < count; i++) {
+          const calculated = new Int32Array(count);
+          for (let i = 0; i < count; i++) {
             calculated[i] = buf.readInt32BE(pos);
             pos += 4;
           }
@@ -167,27 +162,25 @@ Luxtronik2.prototype = {
           that.log.debug('Current temperature is: %s', temperature);
           callback(null, temperature);
         }
-			} else {
+      } else {
         that.log.warn('Error: Luxtronik2 did not confirm command to send data.');
-        // stop();
       }
 
       luxsock.end();
-		});
-		// connected => get values
-		luxsock.on('connect', function () {
+    });
+    luxsock.on('connect', function () {
       luxsock.setNoDelay(true);
-			luxsock.setEncoding('binary');
+      luxsock.setEncoding('binary');
       const {Buffer} = require('node:buffer');
       const buf = Buffer.alloc(4);
-			buf.writeUInt32BE(3004, 0);
-			luxsock.write(buf.toString('binary'), 'binary');
-			buf.writeUInt32BE(0, 0);
-			luxsock.write(buf.toString('binary'), 'binary');
-		});
+      buf.writeUInt32BE(3004, 0);
+      luxsock.write(buf.toString('binary'), 'binary');
+      buf.writeUInt32BE(0, 0);
+      luxsock.write(buf.toString('binary'), 'binary');
+    });
   },
 
-  getCurrentTemperature(callback) { // Wrapper for service call to early return
+  getCurrentTemperature(callback) {
     const counter = ++this.counter;
     this.log.debug('getCurrentTemperature: early callback with cached CurrentTemperature: %s (%d).', this.CurrentTemperature, counter);
     callback(null, this.CurrentTemperature);
@@ -199,32 +192,30 @@ Luxtronik2.prototype = {
   },
 
   identify: function (callback) {
-		this.log.info('Currently there is no way to help identify the Luxtronik2 device!');
-		callback();
-	},
+    this.log.info('Currently there is no way to help identify the Luxtronik2 device!');
+    callback();
+  },
 
-	getServices: function () {
+  getServices: function () {
     const informationService = new Service.AccessoryInformation();
     informationService
-    .setCharacteristic(Characteristic.Manufacturer, 'homebridge-luxtronik2')
-    .setCharacteristic(Characteristic.Model, 'Luxtronik2')
-    .setCharacteristic(Characteristic.FirmwareRevision, this.firmwareRevision)
-    .setCharacteristic(Characteristic.SerialNumber, this.name);
+      .setCharacteristic(Characteristic.Manufacturer, 'homebridge-luxtronik2')
+      .setCharacteristic(Characteristic.Model, 'Luxtronik2')
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmwareRevision)
+      .setCharacteristic(Characteristic.SerialNumber, this.name);
     this.temperatureService
-    .getCharacteristic(Characteristic.CurrentTemperature)
-    .setProps({
-      minValue: Number.parseFloat('-50'),
-      maxValue: Number.parseFloat('100'),
-              })
-    .on('get', this.getCurrentTemperature.bind(this));
-
-//
+      .getCharacteristic(Characteristic.CurrentTemperature)
+      .setProps({
+        minValue: Number.parseFloat('-50'),
+        maxValue: Number.parseFloat('100'),
+      })
+      .on('get', this.getCurrentTemperature.bind(this));
 
     return [informationService, this.temperatureService];
-	},
+  },
 };
 
-module.exports = function (homebridge) { /* unicorn/no-anonymous-default-export: ["off"] */
+module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   homebridge.registerAccessory('homebridge-luxtronik2', 'temperature', Luxtronik2);
